f21ed80fec1ec2aa8104669293387d542418eca8
Lujivillarreal <115663037+Lujivillarreal@users.noreply.github.com>
2022-11-14 20:17:33 -0300

    Update proyecto calculadora.py

--- proyecto calculadora.py
+++ proyecto calculadora.py
@@ -1,43 +1,25 @@
+operacion= "SUMAR","RESTA","MULTIPLICAR","DIVIDIR"
+SUMAR="a"
+RESTAR="b"
+MULTIPLICAR= "c"
+DIVIDIR= "d"
 
-'''
-S:SUMA
-R:RESTA
-M:MULTIPLICACION
-D:DIVISION
-'''
-
-
-def SUMA():
-    num1=input("ingrese un numero:")
-    num2= input("ingrese otro numero:")
+input('Bienvenido')
+num1=input("ingrese un numero:")
+num2= input("ingrese otro numero:") 
+print ("indique la operacion que desea realizar: ")
+print("a->SUMAR")
+print("b->RESTAR") 
+print("c->MULTIPLICAR")    
+print("d->DIVIDIR")    
+operacion= input("indique la operacion que desea realizar: ")
+if operacion=="SUMAR" :
     print(num1,"+", num2,"=", num1+ num2)
-
-def RESTA():
-    num1=input("ingrese un numero:")
-    num2= input("ingrese otro numero:")
+elif operacion=="RESTAR" :  
     print(num1,"-", num2,"=", num1-num2)
-  
-def MULTIPLICACION():
-    num1=input("ingrese un numero:")
-    num2= input("ingrese otro numero:")
-    print(num1,"*", num2,"=", num1*num2)    
-
-def DIVISION():
-    num1=input("ingrese un numero:")
-    num2= input("ingrese otro numero:")
-    print(num1,"/", num2,"=", num1/num2)  
-    
-
-input('Bienvenido') 
-while True:      
-    Operacion= int (input())
-    if  Operacion=="SUMA " :
-        SUMA()
-    elif Operacion=="RESTA" :  
-        RESTA()
-    elif Operacion== "MULTIPLICACION":
-        MULTIPLICACION()
-    elif Operacion== "DIVISON":
-        DIVISION()
+elif operacion== "MULTIPLICAR":
+    print(num1,"*", num2,"=", num1*num2)
+elif operacion== "DIVIDIR":
+    print(num1,"/", num2,"=", num1/num2)
 
 
