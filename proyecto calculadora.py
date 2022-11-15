1d7d408d89407870026090843295668176d99bf1
Lujivillarreal <115663037+Lujivillarreal@users.noreply.github.com>
2022-11-14 21:01:56 -0300

    Update proyecto calculadora.py

--- proyecto calculadora.py
+++ proyecto calculadora.py
@@ -8,18 +8,21 @@ input('Bienvenido')
 num1=input("ingrese un numero:")
 num2= input("ingrese otro numero:") 
 print ("indique la operacion que desea realizar: ")
-print("a->SUMAR")
-print("b->RESTAR") 
-print("c->MULTIPLICAR")    
-print("d->DIVIDIR")    
+print("a=>SUMAR")
+print("b=>RESTAR") 
+print("c=>MULTIPLICAR")    
+print("d=>DIVIDIR")    
 operacion= input("indique la operacion que desea realizar: ")
 if operacion=="SUMAR" :
-    print(num1,"+", num2,"=", num1+ num2)
+    print(num1,"+",num2,"=", num1+num2)
 elif operacion=="RESTAR" :  
-    print(num1,"-", num2,"=", num1-num2)
+    print(num1,"-",num2, "=", int(num1)- int (num2))
 elif operacion== "MULTIPLICAR":
-    print(num1,"*", num2,"=", num1*num2)
+    print(num1,"x",num2,"=", int(num1)*int(num2))
 elif operacion== "DIVIDIR":
-    print(num1,"/", num2,"=", num1/num2)
+    print(num1,"/",num2,"=", int(num1)/int(num2))
+
+
+
 
 
